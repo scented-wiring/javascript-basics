dc91b1795c7c91caf6251447986ba3c2efcff129
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-06-24 23:38:07 +0100

    updated code

--- src/booleans.js
+++ src/booleans.js
@@ -1,62 +1,133 @@
 const negate = a => {
-  // your code here
+  return a === false;
 };
 
 const both = (a, b) => {
-  // your code here
+  if (a && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const either = (a, b) => {
-  // your code here
+  if (a || b === true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const none = (a, b) => {
-  // your code here
+  if (a === false && b === false) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const one = (a, b) => {
-  // your code here
+  if (a === true && b === false) {
+    return true;
+  } else if (a === false && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const truthiness = a => {
-  // your code here
+  if (!a) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  if (a > b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  if (a <= b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isOdd = a => {
-  // your code here
+  if (a%2 > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isEven = a => {
-  // your code here
+  if (a%2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isSquare = a => {
-  // your code here
+  if (Math.sqrt(a) % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  if (string.charAt(0) === char) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const containsVowels = string => {
-  // your code here
+  if (
+    string.includes("a") ||
+    string.includes("e") ||
+    string.includes("i") ||
+    string.includes("o") ||
+    string.includes("u") ||
+    string.includes("A") ||
+    string.includes("E") ||
+    string.includes("I") ||
+    string.includes("O") ||
+    string.includes("U") === true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isLowerCase = string => {
-  // your code here
+  if (string === string.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 };
+  
+
 
 module.exports = {
   negate,
